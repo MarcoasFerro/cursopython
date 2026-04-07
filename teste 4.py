c399a21b5c7d86816eb93d2db02051081da5e921
Marcoasferro <marcquinhos2504@gmail.com>
2026-04-07 09:39:52 -0300

    Adicionando novos arquivos de teste

---
+++ teste 4.py
@@ -0,0 +1,3 @@
+nome = input('digite seu nome:')
+cidade = input('digite sua cidade:')
+print('Olá', nome, 'que legal que você mora em', cidade)
---
+++ teste 5.py
@@ -0,0 +1,4 @@
+nome = input('digite seu nome:')
+anoatual = input('digite o ano atual')
+datanasc = input('digite a sua data de nascimento')
+print(nome, 'em', anoatual, 'você ja faz ou fez aniversário')
